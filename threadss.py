5ce2a72e43f4f161e37a838a448b662484f2bad8
Анастасия Соловьева <solovevaanastasiya31@gmail.com>
2023-11-26 17:51:19 +0300

    Fixed all issues

--- threadss.py
+++ threadss.py
@@ -3,9 +3,10 @@ import time
 import psutil
 import pandas as pd
 
+
 def get_cpu_temp(date, time_list, queuee):
     while True:
-        time.sleep(2)
+        time.sleep(60)
         date.append(psutil.cpu_stats().ctx_switches)
         time_list.append(f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}")
         queuee.append("Число переключений контекста")
@@ -13,35 +14,37 @@ def get_cpu_temp(date, time_list, queuee):
 
 def memory(mem_date, time_list, queuee):
     while True:
-        time.sleep(2)
+        time.sleep(60)
         mem_date.append(psutil.virtual_memory().percent)
         time_list.append(f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}")
         queuee.append("Процент использования памяти, проценты")
 
 
-time_list = []
+def collecter(work_time: float):
+    time_list = []
 
-date = []
-queuee = []
-t1 = threading.Thread(target=get_cpu_temp, args=(date, time_list, queuee, ), daemon=True)
-t2 = threading.Thread(target = memory, args=(date,time_list, queuee, ), daemon=True)
+    date = []
+    queuee = []
+    t1 = threading.Thread(target=get_cpu_temp, args=(date, time_list, queuee,), daemon=True)
+    t2 = threading.Thread(target=memory, args=(date, time_list, queuee,), daemon=True)
 
-t1.start()
-t2.start()
+    t1.start()
+    t2.start()
 
-work_time = 15
-delta = 0
-start_time = time.time()
-while (delta <= work_time):
-    delta = time.time() - start_time
+    delta = 0
+    start_time = time.time()
+    while (delta <= work_time):
+        delta = time.time() - start_time
 
+    t1.join(0.1)
+    t2.join(0.1)
 
-t1.join(0.1)
-t2.join(0.1)
+    print(time_list)
+    print(queuee)
+    print(date)
 
-print(time_list)
-print(queuee)
-print(date)
+    table = pd.DataFrame({"Время": time_list, "Процесс": queuee, "Данные": date})
+    table.to_csv("table.csv", sep=";")
 
-table = pd.DataFrame({"Время":time_list, "Процесс":queuee, "Данные":date})
-table.to_csv("table.csv", sep=";")
+if __name__ == '__main__':
+    collecter(125)
